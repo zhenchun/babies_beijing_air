7d842986bdad793b47d9f070e063f0acf87c3e60
zhenchun <zy037@yahoo.com>
2020-11-10 16:43:29 +0800

    add two_weeks_data_extraction.R

--- two_weeks_data_extraction.R
+++ two_weeks_data_extraction.R
@@ -1,6 +1,7 @@
-library(readr) #加载读取excel包
+library(readr) 
 library(stringr)
 library(lubridate)
+library(dplyr)
 
 pollutants<-c("NO2", "SO2")
 
@@ -35,14 +36,14 @@ for(j in 1: length(uniq)){
 
 for (p in pollutants){
 
-dat<- read_csv(paste0("C:/Users/zy125/Box Sync/Postdoc/BABIES/exposure_data_v2/babies_pred_",p,".csv")) #a是导入的数据表新命名名称
-a<-dat[,-1] #删除第一列序号，不用重复
-bb2=data.frame() #创建bb2空的数据集件
+dat<- read_csv(paste0("C:/Users/zy125/Box Sync/Postdoc/BABIES/exposure_data_v2/babies_pred_",p,".csv")) 
+a<-dat[,-1] 
+bb2=data.frame() 
 
-bb1=data.frame() #创建一个空的bb1数据集
-collast<-as.numeric(str_sub(colnames(a),7,7)) #末位数字提取，根据末位数字判断复制次数
-collast3<-as.numeric(str_sub(colnames(a),3,5)) #中间3位数字提取
-#复制第一列
+bb1=data.frame() 
+collast<-as.numeric(str_sub(colnames(a),7,7)) 
+collast3<-as.numeric(str_sub(colnames(a),3,5)) 
+
    if(collast3[1]==1){
        if(collast3[1]==collast3[2]){
        bb1<-data.frame(rep(a[1],each=collast[2]-collast[1]))
@@ -54,9 +55,9 @@ collast3<-as.numeric(str_sub(colnames(a),3,5)) #中间3位数字提取
       }
     }
 bb1=data.frame()
-#复制第二列到最后一列
-for(i in 2:length(colnames(a))) { #对已有列进行遍历
-  if(!is.na(collast[i+1])){ #重复除最后一列数据外的所有列
+
+for(i in 2:length(colnames(a))) { 
+  if(!is.na(collast[i+1])){ 
     if(collast3[i]==collast3[i+1]){
       bb1<-data.frame(rep(a[i],each=collast[i+1]-collast[i]))
       bb2<-data.frame(append(bb2,bb1))
@@ -64,13 +65,13 @@ for(i in 2:length(colnames(a))) { #对已有列进行遍历
     else{
       if(collast3[i]==collast3[i-1]){
         bb1<-data.frame(rep(a[i],each=7-collast[i])) 
-        bb2<-data.frame(append(bb2,bb1))} #写入新的数据集bb2
+        bb2<-data.frame(append(bb2,bb1))} 
       else if(collast3[i]!=collast3[i-1]){
         bb1<-data.frame(rep(a[i],each=6)) 
-        bb2<-data.frame(append(bb2,bb1))} #写入新的数据集bb2
+        bb2<-data.frame(append(bb2,bb1))} 
     }
   }
-  else{ #重复最后一列数据
+  else{ 
     if(collast3[i]!=collast3[i-1]){
       bb1<-data.frame(rep(a[i],each=6))
       bb2<-data.frame(append(bb2,bb1))
